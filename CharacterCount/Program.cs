cacbad5d99c50a1faadaefc007ac3a98cf4e65bb
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-11-16 10:33:24 +0100

    Initial commit

---
+++ CharacterCount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CharacterCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            string text = "something";
+            int numberOfLetters = 0;
+            int leftPadding = 4;
+
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                string inText = Console.ReadLine();
+                text = inText.ToLower();
+
+                foreach (var character in text ?? string.Empty)
+                {
+                    numberOfLetters++;
+                    counts[(int)character]++;
+                }
+
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        var charPercent = 100 * (double)counts[i] / numberOfLetters;
+                        Console.WriteLine(character + " - " 
+                            + counts[i].ToString().PadLeft(leftPadding) + " " 
+                            + charPercent.ToString("F1").PadLeft(leftPadding) + "%");
+                    }
+                }
+            }
+        }
+    }
+}
